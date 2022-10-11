e45baee69f429b2e46df9cc1e2df60d5d3592c99
jackie123-dotcom <115568922+jackie123-dotcom@users.noreply.github.com>
2022-10-11 15:34:50 -0700

    try gitignore

---
+++ index.js
@@ -0,0 +1,10 @@
+
+function buttonClick(params) {
+    console.log("Did you clicke me?");
+    document.getElementById("idName").style.color="Red";
+    document.getElementById("idName").style.fontSize="50px";
+    
+
+}
+//buttonClick()
+// console.log("welcome");
